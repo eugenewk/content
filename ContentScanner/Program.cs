586a1e24b775c592b3506365c10eccbfe84f1330
eugenewk <eugene.kayal@gmail.com>
2016-08-17 20:53:48 -0700

    now scanning content

--- ContentScanner/Program.cs
+++ ContentScanner/Program.cs
@@ -20,23 +20,61 @@ namespace ContentScanner
             {
                 //get current directory
                 string path = Directory.GetCurrentDirectory();
+                // string path = @"C:\Users\Eugene\Desktop\beats";
+
                 string filename = "ContentScanResults.txt";
+
+                string file = path + @"\" + filename;
 
                 using (StreamWriter sw = File.CreateText(path + "\\" + filename))
                 {
-                    sw.WriteLine("Hello");
-                    sw.WriteLine("And");
-                    sw.WriteLine("Welcome");
+                    sw.WriteLine("Filename\tPath\tSize");
+                    Scanner(sw, path);
                 }
+
+                Console.WriteLine("Scan complete");
+                Console.ReadKey();
             }
             catch (Exception e)
             {
                 Console.WriteLine("The process failed: {0}", e.ToString());
                 Console.ReadKey();
             }
+        }
 
-            Console.WriteLine("Successfully executed");
-            Console.ReadKey();
+        static void Scanner(StreamWriter sw, string current_dir)
+        {
+            // note file is already open for writing
+
+            // get all subdirectories in current path, store for later
+            string[] dirs = Directory.GetDirectories(current_dir, "*", SearchOption.TopDirectoryOnly);
+            foreach (string dir in dirs)
+            {
+                sw.WriteLine("Folder\tdir\t-");
+
+                Console.WriteLine(dir);
+                Scanner(sw, dir);
+            }
+            
+            // get all files
+            string[] files = Directory.GetFiles(current_dir, "*", SearchOption.TopDirectoryOnly);
+            foreach (string file in files)
+            {
+                string filename = Path.GetFileName(file);
+                long filesize = new System.IO.FileInfo(file).Length;
+                sw.WriteLine("{0}\t{1}\t{2}", filename, current_dir, filesize);
+
+                Console.WriteLine(file);
+            }
+
+            // for each file, write name, path, and size
+           /* foreach (string file in files)
+            {
+                
+            }*/
+
+
+            // for each subdirectory, recurse
         }
     }
 }
